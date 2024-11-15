8439f315d30cc28fdabc3c1a956b4d86f5c7824e
faezehabdolazimi <75137625+faezehabdolazimi@users.noreply.github.com>
2024-11-15 07:37:32 +0330

    Create SELECT.sql

---
+++ SELECT.sql
@@ -0,0 +1,11 @@
+SELECT *  
+FROM employees 
+WHERE department_name = '1';
+
+SELECT *  
+FROM employees  
+WHERE department_name = '1' AND salary > 100;
+
+SELECT *  
+FROM employees  
+ORDER BY name ASC;
